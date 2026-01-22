d3293ee3f40b3a0b5953ff204c31ec5ea47ad17b
shaik afnan <skafnan06@gmail.com>
2026-01-22 06:37:28 +0000

    doneee

---
+++ phase-1/21jan-assign-js/inc-dec.js
@@ -0,0 +1,86 @@
+//let a = 5
+//console.log(a++)
+//console.log(a)
+
+//First output: 5  
+//Second output: 6
+
+
+//let b = 5
+//console.log(++b)
+//console.log(b)
+
+//First output: 6  
+//Second output: 6
+
+
+//let c = 10
+//console.log(c--)
+//console.log(c)
+
+//First output: 10  
+//Second output: 9
+
+//let d = 10
+//console.log(--d)
+//console.log(d)
+
+//First output: 9  
+//Second output: 9
+
+
+//let x = 7
+//let y = x++ + 5
+//console.log(y)
+//console.log(x)
+
+//y = 12  
+//x = 8
+
+
+//let x = 7
+//let y = ++x + 5
+//console.log(y)
+//console.log(x)
+
+//y = 13  
+//x = 8
+
+
+//let num = 3
+//console.log(num++)
+//console.log(num++)
+//console.log(num++)
+//console.log(num)
+
+//First output: 3  
+//Second output: 4  
+//Third output: 5  
+//Fourth output: 6  
+
+
+//let a = 5
+//let result = a++ + ++a
+//console.log(result)
+//console.log(a)
+
+//result = 12  
+//a = 7
+
+
+//let p = 20
+//let q = --p + p--
+//console.log(q)
+//console.log(p)
+
+//q = 38  
+//p = 18
+
+
+//let m = 10
+//let n = m++ + m + ++m
+//console.log(n)
+//console.log(m)
+
+//n = 33  
+//m = 12
